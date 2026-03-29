e6bc94cad6a5d8449c99aec73c9f7bcaf0f92fc3
sandy.saryono <sandy.saryono@gmail.com>
2026-03-30 06:47:20 +0700

    perbaikan head meta

--- resources/views/layouts/frontend/head-css.blade.php
+++ resources/views/layouts/frontend/head-css.blade.php
@@ -1,13 +1,12 @@
 <meta charset="utf-8">
-<title>Yayasan Sains Nusantara | Lembaga Penelitian & Teknologi Terapan Indonesia
-</title>
+<title>Yayasan Sains Nusantara | Penelitian & Teknologi Terapan Indonesia</title>
 
 <!-- mobile responsive meta -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="description"
-    content="Yayasan Sains Nusantara adalah lembaga nirlaba yang fokus pada penelitian, pelatihan, dan rekayasa teknologi terapan untuk kesejahteraan masyarakat Indonesia.
-">
+    content="YSN menghadirkan inovasi teknologi terapan melalui penelitian dan pelatihan untuk mendukung kesejahteraan masyarakat Indonesia. Jelajahi program kami.">
+<meta name="robots" content="index, follow">
+
 <!-- ** Plugins Needed for the Project ** -->
 <!-- Bootstrap -->
 <link rel="stylesheet" href="{{ asset('assets/fe/plugins/bootstrap/bootstrap.min.css') }}">
@@ -30,5 +29,8 @@
 <!--Favicon-->
 <link rel="shortcut icon" href="{{ asset('assets/fe/images/favicon.ico') }}" type="image/x-icon">
 <link rel="icon" href="{{ asset('assets/fe/images/favicon.ico') }}" type="image/x-icon">
+
+
+<link rel="canonical" href="https://ysn.or.id/">
 
 @stack('styles')
